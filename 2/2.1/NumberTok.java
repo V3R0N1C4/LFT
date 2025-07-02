0fd61633362ed4da0cffddaae4ce3fe844e4eb1c
Veronica Bosso <120654473+V3R0N1C4@users.noreply.github.com>
2025-07-02 13:32:30 +0000

    Implementazione del lexer e delle classi di token per la scansione lessicale

---
+++ 2/2.1/NumberTok.java
@@ -0,0 +1,12 @@
+public class NumberTok extends Token {
+    public String lexeme = " ";
+
+    public NumberTok(int tag, String s) {
+		    super(tag); //eredito tag da Token
+        lexeme = s;
+	  }
+
+    public String toString() {
+		    return "<" + tag + ", " + lexeme + ">";
+	  }
+}
